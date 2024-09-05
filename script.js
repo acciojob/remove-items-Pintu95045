14e66296f46059f613738d50f3da858bcb6752c8
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-09-05 23:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', removeSelectedColor);
+
+function removeSelectedColor() {
+    const colorSelect = document.getElementById('colorSelect');
+    if (colorSelect.selectedIndex !== -1) { // Check if an item is selected
+        colorSelect.remove(colorSelect.selectedIndex);
+    }
+}
